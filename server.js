72035e2589cae5f82966665f9d191436d98963bb
pedro-drosa <pedro_drosa@hotmail.com>
2022-03-24 17:41:34 -0300

    env: i changed the application port to facilitate deployment
    - the port was fixed with a default number

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 import app from './app.js';
 
-const { APP_PORT } = process.env;
+const APP_PORT = process.env.PORT || process.env.APP_PORT;
 const { APP_BASE_URL } = process.env;
 
-app.listen('5000', () => {
+app.listen(APP_PORT, () => {
   console.log(`⚡ server is running on ${APP_BASE_URL}:${APP_PORT}`);
 });
